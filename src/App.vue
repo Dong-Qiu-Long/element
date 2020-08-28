147d3f21f81498b08b7b3b1c4fcdf005d221baa3
Dong-Qiu-Long <1902941357@qq.com>
2020-08-28 17:09:24 +0800

    8.28

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,94 @@
 <template>
   <div id="app">
-    <el-row></el-row>
-    <el-col></el-col>
-    我是董秋龙
+    <h1>基础布局</h1>
+    <el-row class="hidden-xs-only">
+      <el-col>
+        <div class="bg-purple-dark bg-purple grid-content"></div>
+      </el-col>
+    </el-row>
+    <h1>分栏布局</h1>
+    <el-row :gutter='20'>
+      <el-col :span='4'>
+        <div class="bg-purple-dark bg-purple grid-content"></div>
+      </el-col>
+      <el-col :span='4'>
+        <div class="bg-purple-dark bg-purple grid-content"></div>
+      </el-col>
+      <el-col :span='4'>
+        <div class="bg-purple-dark bg-purple grid-content"></div>
+      </el-col>
+      <el-col :span='4'>
+        <div class="bg-purple-dark bg-purple grid-content"></div>
+      </el-col>
+      <el-col :span='4'>
+        <div class="bg-purple-dark bg-purple grid-content"></div>
+      </el-col>
+      <el-col :span='4'>
+        <div class="bg-purple-dark bg-purple grid-content"></div>
+      </el-col>
+    </el-row>
+    <h1>混合布局</h1>
+    <el-row :gutter="20">
+      <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <h1>分栏偏移</h1>
+    <el-row :gutter="20">
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="6" :push='6'><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span='6' :offset="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span='6' :pull='6'><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <h1>对齐方式</h1>
+    <el-row type="flex" class="row-bg" align="middle">
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="center" align="bottom">
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="end">
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="space-between">
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="space-around">
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <h1>响应式布局</h1>
+    <el-row :gutter="10">
+      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
   </div>
 </template>
 
@@ -20,12 +106,40 @@ export default {
 </script>
 
 <style lang="scss">
+*{
+  padding: 0px;
+  margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+ .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
--- src/components/element/Row.vue
+++ src/components/element/Row.vue
@@ -1,3 +1,79 @@
 <template>
-	<div class="el-row">我是row</div>
+	<div
+		:class="[
+			'el-row',
+			{'el-row-flex' : this.type === 'flex'},
+			this.justify !== 'start' && `is-justify-${justify}`,
+			this.align !== 'top' && `is-align-${align}`
+			]" :style="style">
+		<slot></slot>
+	</div>
 </template>
+<script>
+export default {
+  name: 'ElRow',
+  props: {
+    gutter: {
+      type: Number,
+      default: 0,
+    },
+    type: String,
+    justify: {
+      type: String,
+      default: 'start',
+      validator: (val) => ['start', 'end', 'center', 'space-between', 'space-around'].includes(val),
+    },
+    align: {
+      type: String,
+      default: 'top',
+      validator: (val) => ['top', 'middle', 'bottom'].includes(val),
+
+    },
+  },
+  computed: {
+    style() {
+      const style = {};
+      style.marginLeft = `${-this.gutter}px`;
+      style.marginRight = style.marginLeft;
+      return style;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+	.el-row{
+		&::after{
+			display: block;
+			content: '';
+			visibility: hidden;
+			clear: both;
+			height: 0;
+		}
+		&-flex{
+			display: flex;
+			&::after,
+			&::before{
+				display: none;
+			}
+			&.is-justify-end{
+				justify-content: flex-end;
+			}
+			&.is-justify-ecenter{
+				justify-content: center;
+			}
+			&.is-justify-space-between{
+				justify-content: space-between;
+			}
+			&.is-justify-space-around{
+				justify-content: space-around;
+			}
+			&.is-align-middle{
+				align-items: center;
+			}
+			&.is-align-bottom{
+				align-items: flex-end;
+			}
+		}
+		box-sizing: border-box;
+	}
+</style>
